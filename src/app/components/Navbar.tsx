5c28f97516355e6a85935f093477cd8c2f30a762
Shehryar <shehryar7ahmed@gmail.com>
2024-06-14 23:28:54 +0500

    feat: add join our codex

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -10,8 +10,10 @@ export default function Navbar() {
     <nav className="flex justify-end space-x-4">
   <p className="hover:underline cursor-pointer"  onClick={() => scrollToElement('worked-with')}>Worked with</p>
   <p className="hover:underline cursor-pointer" onClick={() => scrollToElement('services')} >Services</p>
-  <p className="hover:underline cursor-pointer " onClick={() => scrollToElement('contact')}>Consultancy</p>
+  {/* <p className="hover:underline cursor-pointer " onClick={() => scrollToElement('contact')}>Consultancy</p> */}
   <p className="hover:underline cursor-pointer" onClick={() => scrollToElement('contact')}  >Hire</p>
+  <p className="underline cursor-pointer" onClick={() =>     window.location.href = 'https://forms.gle/g8JhteWwLfs9LiYf6'
+}  >Join our CODEX Cohort</p>
 
 </nav>
 
